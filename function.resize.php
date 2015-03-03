bc8e54bd2cd4daabb7de93f54dc9bc93c1a805ea
Rubén Antón <rubocoptero@gmail.com>
2015-03-03 16:02:37 +0100

    Delete isInCache function because it has been moved to a class already

--- function.resize.php
+++ function.resize.php
@@ -4,20 +4,6 @@ require 'ImagePath.php';
 require 'Configuration.php';
 require 'Resizer.php';
 
-function isInCache($path, $imagePath) {
-    $isInCache = false;
-    if(file_exists($path) == true):
-        $isInCache = true;
-        $origFileTime = date("YmdHis",filemtime($imagePath));
-        $newFileTime = date("YmdHis",filemtime($path));
-        if($newFileTime < $origFileTime): # Not using $opts['expire-time'] ??
-            $isInCache = false;
-        endif;
-    endif;
-
-    return $isInCache;
-}
-
 function resize($originalPath,$opts=null){
 	$image = new ImagePath($originalPath);
     try {
