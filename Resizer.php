047fd65ee2c3e85a67efff7ad1b45bca95004633
Rubén Antón <rubocoptero@gmail.com>
2015-03-16 14:06:27 +0100

    Moved obtainFilePath method to ImagePath object

--- Resizer.php
+++ Resizer.php
@@ -8,10 +8,10 @@ class Resizer {
     private $configuration;
     private $fileSystem;
 
-    public function __construct($path, $configuration) {
-        $this->checkPath($path);
+    public function __construct($image, $configuration) {
+        $this->checkImage($image);
         $this->checkConfiguration($configuration);
-        $this->image = $path;
+        $this->image = $image;
         $this->configuration = $configuration;
         $this->fileSystem = new FileSystem();
     }
@@ -20,11 +20,33 @@ class Resizer {
         $this->fileSystem = $fileSystem;
     }
 
-    private function checkPath($path) {
+    private function checkImage($path) {
         if (!($path instanceof ImagePath)) throw new InvalidArgumentException();
     }
 
     private function checkConfiguration($configuration) {
         if (!($configuration instanceof Configuration)) throw new InvalidArgumentException();
+    }
+
+    function composeNewPath($imagePath, $configuration) {
+        $w = $configuration->obtainWidth();
+        $h = $configuration->obtainHeight();
+        $filename = md5_file($imagePath);
+        $finfo = pathinfo($imagePath);
+        $ext = $finfo['extension'];
+
+        $cropSignal = isset($opts['crop']) && $opts['crop'] == true ? "_cp" : "";
+        $scaleSignal = isset($opts['scale']) && $opts['scale'] == true ? "_sc" : "";
+        $widthSignal = !empty($w) ? '_w'.$w : '';
+        $heightSignal = !empty($h) ? '_h'.$h : '';
+        $extension = '.'.$ext;
+
+        $newPath = $configuration->obtainCache() .$filename.$widthSignal.$heightSignal.$cropSignal.$scaleSignal.$extension;
+
+        if($opts['output-filename']) {
+            $newPath = $opts['output-filename'];
+        }
+
+        return $newPath;
     }
 }
